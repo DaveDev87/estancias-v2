843728afcc27e88007376eb4be0d8e3b288f4fc4
dave <alexruedarivas@gmail.com>
2022-06-03 11:16:11 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-red-400">
+    {{counter}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+
+}
+</script>
